d5f3204518a3d562cdcd711ab26c99e5e97e76d8
juliocesardeoliveiraecia-del <juliocesardeoliveiraecia@gmail.com>
2026-04-16 15:51:55 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lumen-casa-v1';
+const CACHE = 'lumen-casa-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
